a108bc3a39e74da9ac7c61b470a76f9d9dec03c3
Wesley Oliveira <wesleyholiveir@gmail.com>
2017-11-08 15:54:26 -0200

    Updated glide

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/wesleyholiveira/handlers"
 	"github.com/wesleyholiveira/middlewares"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+const (
+	APP_ADDR = ""
+	APP_PORT = 8080
 )
 
 
 func main() {
+	// full address
+	var fullAddr string = fmt.Sprintf("%s:%d", APP_ADDR, APP_PORT)
+
 	// retrieve instance router of gorilla
 	r := mux.NewRouter()
+
+	log.Println("Iniciando o servidor (" + fullAddr + ")")
+
+	db, err := gorm.Open("mysql", "root:zarp001@localhost/openstock")
+
+	if err != nil {
+		db.Close()
+		log.Fatal("Ocorreu um problema ao conectar ao banco")
+	}
 
 	// register the routes
 	r.HandleFunc("/", handlers.MainHandler)
@@ -19,5 +39,5 @@ func main() {
 	// register the Handler
 	http.Handle("/", middlewares.GeneralMiddleware(r))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fullAddr, nil))
 }
